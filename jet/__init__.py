0b6334bdead425608d1eaee7123d0b4b5b2d624d
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-24 19:33:05 +0800

    Updates

--- jet/__init__.py
+++ jet/__init__.py
@@ -1,6 +1,3 @@
-# Setup all shared packages
-from .shared_modules import *
-
 # Enable parallelism for faster LLM tokenizer encoding
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
--- jet/shared_modules/shared/events/events.py
+++ jet/shared_modules/shared/events/events.py
@@ -106,6 +106,12 @@ __all__ = [
 ]
 
 if __name__ == "__main__":
+    from jet.logger.config import configure_logger
+    from shared.events import EventSettings
+
+    # Trigger event event_pre_start_hook
+    EventSettings.pre_start_hook(configure_logger)
+
     logger.newline()
     pre_start_hook_start_time = EventSettings.event_data["pre_start_hook"]["start_time"]
     logger.info("pre_start_hook start time:")
