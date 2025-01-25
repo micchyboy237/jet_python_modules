60714e3bd55490ab0163bb5c83afe7607128840c
Jethro Estrada <jethroestrada237@gmail.com>
2025-01-26 04:57:16 +0800

    Updates

--- jet/__init__.py
+++ jet/__init__.py
@@ -6,4 +6,5 @@ import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 # Trigger event event_pre_start_hook
-EventSettings.pre_start_hook(configure_logger)
+# EventSettings.pre_start_hook(configure_logger)
+EventSettings.pre_start_hook()
