26fbdd7b8cc76a4458eb2402184e52553ac16969
Jethro Estrada <jethroestrada237@gmail.com>
2025-02-28 07:02:43 +0800

    Updates

--- jet/libs/txtai/pipeline/lemmatizer.py
+++ jet/libs/txtai/pipeline/lemmatizer.py
@@ -14,37 +14,12 @@ def lemmatize_text(text: str) -> list[str]:
     return [lemmatizer.lemmatize(token) for token in tokens]
 
 
-# import string
 # import spacy
 
 # nlp = spacy.load("en_core_web_sm")
 
 
-# def lemmatize_text(text: str, include_puncts=[':', '.', '?', '!', '-', '/']) -> list[str]:
+# def lemmatize_text(text: str) -> list[str]:
 #     text = unidecode.unidecode(text)
 #     doc = nlp(text)
-
-#     lemmas = []
-#     for i, token in enumerate(doc):
-#         token_text = token.text
-#         chars = list(token_text)
-#         unique_chars = list(set(token_text))
-
-#         if chars[0] in string.punctuation:
-#             token = chars[0]
-#             if token in include_puncts:
-#                 if len(chars) > len(unique_chars):
-#                     token_text = ''
-#                 else:
-#                     token_text = token
-#             elif token not in include_puncts:
-#                 token_text = ''
-
-#             if token == ':':
-#                 token_text = ': '
-#             elif token == '/':
-#                 token_text = ', '
-
-#         lemmas.append(token_text)
-
-#     return lemmas
+#     return [token.text for token in doc]
