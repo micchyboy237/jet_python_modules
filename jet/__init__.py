afeac92acadefd09a9d52ace2b4f058688a71034
Jethro Estrada <jethroestrada237@gmail.com>
2025-05-08 19:38:04 +0800

    Updates

--- jet/__init__.py
+++ jet/__init__.py
@@ -1,7 +1,7 @@
 # Initialize all shared modules
-import shared.setup.builtin_modules
+# import shared.setup.builtin_modules
 
-from shared.setup.events import setup_events
+# from shared.setup.events import setup_events
 
 # Enable parallelism for faster LLM tokenizer encoding
 import os
@@ -9,4 +9,4 @@ import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 # Trigger event initialize
-setup_events()
+# setup_events()
