1138f512d08fa93db6eac99dc3955d59fb98c0c9
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-05 09:44:40 +0800

    Updates

--- jet/models/download_hf_model.py
+++ jet/models/download_hf_model.py
@@ -1,6 +1,6 @@
 from huggingface_hub import snapshot_download
 
-repo_id = "tomaarsen/span-marker-roberta-large-ontonotes5"
+repo_id = "tomaarsen/span-marker-bert-base-fewnerd-fine-super"
 cache_dir = "/Users/jethroestrada/.cache/huggingface/hub"
 snapshot_download(repo_id=repo_id, cache_dir=cache_dir,
                   local_dir_use_symlinks=False, max_workers=4)
