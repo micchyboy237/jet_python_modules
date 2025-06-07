fe504b84f346b58d84dc151fc034dd6f2577145b
Jethro Estrada <jethroestrada237@gmail.com>
2025-06-08 02:36:40 +0800

    Updates

--- jet/models/download_hf_model.py
+++ jet/models/download_hf_model.py
@@ -1,7 +1,8 @@
 from huggingface_hub import snapshot_download
 
-repo_id = "Qwen/Qwen3-Embedding-0.6B"
+repo_id = "mlx-community/Qwen3-1.7B-4bit-DWQ-053125"
 cache_dir = "/Users/jethroestrada/.cache/huggingface/hub"
 snapshot_download(repo_id=repo_id, cache_dir=cache_dir,
-                  local_dir_use_symlinks=False, max_workers=4)
+                  local_dir_use_symlinks=False, max_workers=4,
+                  force_download=True)
 print(f"Model repository downloaded to: {cache_dir}")
