bd31ab7d252e56dfca0954b98e2e80182160da25
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-17 23:45:46 +0800

    Updates

--- jet/scrapers/browser/config.py
+++ jet/scrapers/browser/config.py
@@ -2,9 +2,9 @@
 PLAYWRIGHT_CACHE_DIR = "/Users/jethroestrada/Library/Caches/ms-playwright"
 
 # Specific browser versions
-PLAYWRIGHT_CHROMIUM = f"{PLAYWRIGHT_CACHE_DIR}/chromium-1169"
-PLAYWRIGHT_FIREFOX = f"{PLAYWRIGHT_CACHE_DIR}/firefox-1475"
-PLAYWRIGHT_WEBKIT = f"{PLAYWRIGHT_CACHE_DIR}/webkit-2140"
+PLAYWRIGHT_CHROMIUM = f"{PLAYWRIGHT_CACHE_DIR}/chromium-1181"
+PLAYWRIGHT_FIREFOX = f"{PLAYWRIGHT_CACHE_DIR}/firefox-1489"
+PLAYWRIGHT_WEBKIT = f"{PLAYWRIGHT_CACHE_DIR}/webkit-2191"
 
 # Playwright executable paths (full path to executable)
 PLAYWRIGHT_CHROMIUM_EXECUTABLE = f"{PLAYWRIGHT_CHROMIUM}/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
