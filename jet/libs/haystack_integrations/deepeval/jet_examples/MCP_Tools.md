99e2a5c3b5f2f1ec17a3a3bc03d44dbb58b4c5bc
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-29 00:50:53 +0800

    Updates

---
+++ jet/libs/haystack_integrations/deepeval/jet_examples/MCP_Tools.md
@@ -0,0 +1,31 @@
+Command:
+
+```bash
+python /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/jet/libs/haystack_integrations/mcp/examples/mcp_sse_toolset.py
+```
+
+URL: http://localhost:8931/sse
+
+Discovered 21 tools:
+
+- browser_close: Close the page
+- browser_resize: Resize the browser window
+- browser_console_messages: Returns all console messages
+- browser_handle_dialog: Handle a dialog
+- browser_evaluate: Evaluate JavaScript expression on page or element
+- browser_file_upload: Upload one or multiple files
+- browser_fill_form: Fill multiple form fields
+- browser_install: Install the browser specified in the config. Call this if you get an error about the browser not being installed.
+- browser_press_key: Press a key on the keyboard
+- browser_type: Type text into editable element
+- browser_navigate: Navigate to a URL
+- browser_navigate_back: Go back to the previous page
+- browser_network_requests: Returns all network requests since loading the page
+- browser_take_screenshot: Take a screenshot of the current page. You can't perform actions based on the screenshot, use browser_snapshot for actions.
+- browser_snapshot: Capture accessibility snapshot of the current page, this is better than screenshot
+- browser_click: Perform click on a web page
+- browser_drag: Perform drag and drop between two elements
+- browser_hover: Hover over element on page
+- browser_select_option: Select an option in a dropdown
+- browser_tabs: List, create, close, or select a browser tab.
+- browser_wait_for: Wait for text to appear or disappear or a specified time to pass
---
+++ jet/libs/haystack_integrations/deepeval/jet_examples/browser_take_screenshot.py
@@ -0,0 +1,63 @@
+from haystack.components.agents import Agent
+from haystack.components.generators.utils import print_streaming_chunk
+from haystack.dataclasses import ChatMessage
+# from haystack_integrations.components.generators.google_genai import GoogleGenAIChatGenerator
+from haystack_integrations.tools.mcp import MCPToolset, StreamableHttpServerInfo
+# from ipywidgets import Video
+from jet.adapters.haystack.ollama_chat_generator import OllamaChatGenerator
+from jet.file.utils import save_file
+from jet.logger import logger
+import os
+import shutil
+
+from jet.transformers.formatters import format_json
+
+
+OUTPUT_DIR = os.path.join(
+    os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+log_file = os.path.join(OUTPUT_DIR, "main.log")
+logger.basicConfig(filename=log_file)
+logger.info(f"Logs: {log_file}")
+
+SERVER_INFO = StreamableHttpServerInfo(url="http://localhost:8931/mcp")
+
+SYSTEM_MESSAGE = """
+You are an intelligent assistant equipped with tools for navigating the web.
+
+You can use tools when appropriate, but not every task requires them — you also have strong reasoning and language capabilities.
+If a request seems challenging, don't default to refusal due to perceived tool limitations. Instead, think creatively and attempt a solution using the skills you do have.
+You are more capable than you might assume. Trust your abilities.
+"""
+
+def create_agent(agent_name: str, tool_names: list[str]) -> Agent:
+    toolset = MCPToolset(server_info=SERVER_INFO, tool_names=tool_names)
+    logger.debug("Toolset:")
+    logger.debug(format_json(toolset.tools))
+    chat_generator = OllamaChatGenerator(model="llama3.2", agent_name=agent_name, verbose=False, generation_kwargs={"temperature": 0.1})
+
+    agent = Agent(
+        chat_generator=chat_generator,
+        tools=toolset,
+        system_prompt=SYSTEM_MESSAGE,
+        exit_conditions=["text"],
+        streaming_callback=print_streaming_chunk
+    )
+
+    return agent
+
+def example():
+    tool_names = ["browser_navigate", "browser_take_screenshot"]
+
+    messages = [ChatMessage.from_user("""
+    Navigate to https://www.nbcnews.com and capture a screenshot of the body element and save to full_page.png
+    """)]
+    agent = create_agent("MCP_Agent", tool_names=tool_names)
+    result = agent.run(messages=messages)
+    save_file(result["last_message"].text, f"{OUTPUT_DIR}/results/last_message.md")
+
+if __name__ == "__main__":
+    example()
+
+    logger.info("\n\n[DONE]", bright=True)
