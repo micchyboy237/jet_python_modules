c845b73bd2769e82fddd8c293068e9a407d185f2
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-28 23:13:51 +0800

    Updates

---
+++ jet/libs/haystack_integrations/deepeval/README.md
@@ -0,0 +1,14 @@
+# deepeval-haystack
+
+[![PyPI - Version](https://img.shields.io/pypi/v/deepeval-haystack.svg)](https://pypi.org/project/deepeval-haystack)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/deepeval-haystack.svg)](https://pypi.org/project/deepeval-haystack)
+
+- [Integration page](https://haystack.deepset.ai/integrations/deepeval)
+- [Changelog](https://github.com/deepset-ai/haystack-core-integrations/blob/main/integrations/deepeval/CHANGELOG.md)
+---
+
+## Contributing
+
+Refer to the general [Contribution Guidelines](https://github.com/deepset-ai/haystack-core-integrations/blob/main/CONTRIBUTING.md).
+
+To run integration tests locally, you need to export the `OPENAI_API_KEY` environment variable.
---
+++ jet/libs/haystack_integrations/deepeval/examples/example.py
@@ -0,0 +1,46 @@
+from haystack import Pipeline
+
+from haystack_integrations.components.evaluators.deepeval import DeepEvalEvaluator, DeepEvalMetric
+from deepeval.models import OllamaModel
+
+from jet.logger import logger
+
+QUESTIONS = [
+    "Which is the most popular global sport?",
+    "Who created the Python language?",
+]
+CONTEXTS = [
+    [
+        "The popularity of sports can be measured in various ways, including TV viewership, social media presence, "
+        "number of participants, and economic impact.",
+        "Football is undoubtedly the world's most popular sport with major events like the FIFA World Cup and sports "
+        "personalities like Ronaldo and Messi, drawing a followership of more than 4 billion people.",
+    ],
+    [
+        "Python, created by Guido van Rossum in the late 1980s, is a high-level general-purpose programming language.",
+        "Its design philosophy emphasizes code readability, and its language constructs aim to help programmers write "
+        "clear, logical code for both small and large-scale software projects.",
+    ],
+]
+RESPONSES = [
+    "Football is the most popular sport with around 4 billion followers worldwide",
+    "Python language was created by Guido van Rossum.",
+]
+
+pipeline = Pipeline()
+evaluator = DeepEvalEvaluator(
+    metric=DeepEvalMetric.FAITHFULNESS,
+    metric_params={
+        "model": OllamaModel(model_name="qwen3:4b-q4_K_M"),  # Use Ollama model as the judge LLM
+        "temperature": 0.0,  # Optional: Set low temperature for consistent scoring
+    },
+)
+pipeline.add_component("evaluator", evaluator)
+
+# Each metric expects a specific set of parameters as input. Refer to the
+# DeepEvalMetric class' documentation for more details.
+results = pipeline.run({"evaluator": {"questions": QUESTIONS, "contexts": CONTEXTS, "responses": RESPONSES}})
+
+logger.gray("Results:")
+for output in results["evaluator"]["results"]:
+    logger.success(output)
