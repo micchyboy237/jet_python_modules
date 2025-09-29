17ae291a56743f6a0aed91b7c51313f4f8e0003e
Jethro Estrada <jethroestrada237@gmail.com>
2025-09-29 22:05:59 +0800

    Updates

--- models/mistral/create_model_from_modelfile.ps1
+++ models/mistral/create_model_from_modelfile.ps1
@@ -1,1 +1,2 @@
+# ollama create <model_name> -f <modelfile_path>
 ollama create custom-mistral:7b-tool-v0.3-q3 -f C:\Users\druiv\Desktop\Jet_Files\Jet_Windows_Workspace\ollama_models\mistral-tool\Modelfile
