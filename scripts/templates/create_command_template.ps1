35849eefc4959e9caa9f283b41033b33508b64e8
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-02 13:13:36 +0800

    Updates

--- scripts/templates/create_command_template.ps1
+++ scripts/templates/create_command_template.ps1
@@ -1,3 +1,5 @@
-Set-Content -Path "C:\Users\druiv\Desktop\Jet_Files\Jet_Windows_Workspace\cli_commands\<action>-<FileName>.ps1" -Value @"
+Set-Content -Path "C:\Users\druiv\Desktop\Jet_Files\Jet_Windows_Workspace\cli_commands\<action>-<FileName>.<ext>" -Value @"
+# <action>-<FileName>.<ext>
+
 <add_script_here>
 "@
