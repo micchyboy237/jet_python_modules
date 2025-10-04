82262ca3cdd87b387efd37e6f9be2560fe441d39
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-05 06:40:10 +0800

    Updates

--- reinstall_python.sh
+++ reinstall_python.sh
@@ -1,4 +1,14 @@
-pyenv uninstall -f 3.12.9
-env LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib" \
-    CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix openssl)/include -I$(brew --prefix readline)/include" \
-    pyenv install 3.12.9
+PY_VERSION=3.12.9
+
+pyenv uninstall -f $PY_VERSION
+env \
+  LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib" \
+  CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix openssl)/include -I$(brew --prefix readline)/include" \
+  pyenv install -v $PY_VERSION
+pip install -U pip
+
+echo "Verifying Python and pip versions..."
+python --version
+pip --version
+
+echo "✅ Python $PY_VERSION successfully reinstalled and pip upgraded!"
