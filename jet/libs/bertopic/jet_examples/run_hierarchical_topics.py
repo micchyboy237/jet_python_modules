ad55c9503131f421e2ee9db8046299158a45ccd4
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-12 14:23:45 +0800

    Updates

--- jet/libs/bertopic/jet_examples/run_hierarchical_topics.py
+++ jet/libs/bertopic/jet_examples/run_hierarchical_topics.py
@@ -7,6 +7,8 @@ from jet.libs.bertopic.examples.mock import load_sample_data
 import os
 import shutil
 
+from jet.logger import logger
+
 OUTPUT_DIR = os.path.join(
         os.path.dirname(__file__), "generated", os.path.splitext(os.path.basename(__file__))[0])
 
@@ -20,6 +22,7 @@ def generate_hierarchical_topics(
     docs: List[str],
     method: Literal["ward", "single", "complete", "average", "centroid", "median"] = "ward"
 ):
+    logger.info(f"Generating '{method}' hierarchical topics...")
     output_dir = f"{OUTPUT_DIR}/{method}"
 
     topic_model = BERTopic()
@@ -31,9 +34,14 @@ def generate_hierarchical_topics(
         docs,
         linkage_function=linkage_function
     )
+    tree = topic_model.get_topic_tree(hierarchical_topics)
+
+    logger.debug(f"Method: '{method}' hierarchical tree:")
+    logger.success(tree)
 
     save_file(hierarchical_topics.to_dict(orient="records"), f"{output_dir}/hierarchical_topics.json")
     save_file(topic_model.visualize_hierarchy(hierarchical_topics=hierarchical_topics).to_image(), f"{output_dir}/plots.png")
+    save_file(tree, f"{output_dir}/tree.txt")
 
 # Example Usage
 if __name__ == "__main__":
