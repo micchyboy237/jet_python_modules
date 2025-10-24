fe1c763e48024f5eb7e48fbf63edfb18f89e0647
Jethro Estrada <jethroestrada237@gmail.com>
2025-10-24 15:09:59 +0800

    Updates

---
+++ install_requirements.sh
@@ -0,0 +1,4 @@
+pip install -r /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/requirements-base.txt && \
+pip install -r /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/requirements-utils.txt && \
+pip install -r /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/requirements-local.txt && \
+pip install -r /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_python_modules/requirements-advanced.txt
