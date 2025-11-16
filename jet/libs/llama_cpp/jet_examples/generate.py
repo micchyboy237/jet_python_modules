a4ddec918c3555932a14abfccceed3fc541a6159
Jethro Estrada <jethroestrada237@gmail.com>
2025-11-16 20:36:14 +0800

    Updates

--- jet/libs/llama_cpp/jet_examples/generate.py
+++ jet/libs/llama_cpp/jet_examples/generate.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
+from jet.logger import logger
 
 client = OpenAI(base_url="http://shawn-pc.local:8080/v1", api_key="sk-1234")  # Dummy API key
 response = client.completions.create(
     model="qwen3-instruct-2507:4b",
     prompt="Why is the sky blue?",
 )
-print(response.choices[0].text)
+logger.teal(response.choices[0].text)
