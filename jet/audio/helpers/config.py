df2dff02c74cc2253e02f357fa3f50a29bb16a8f
Jethro Estrada <jethroestrada237@gmail.com>
2026-05-06 15:38:51 +0800

    Updates

--- jet/audio/helpers/config.py
+++ jet/audio/helpers/config.py
@@ -9,12 +9,16 @@ FRAME_LENGTH_S = 0.025
 FRAME_SHIFT_MS = 10
 FRAME_SHIFT_S = 0.010
 
+# Samples per frame (16000 * 25 / 1000 = 400)
 FRAME_LENGTH_SAMPLE = int(SAMPLE_RATE * FRAME_LENGTH_MS / 1000)
+# Hop size in samples (16000 * 10 / 1000 = 160)
 FRAME_SHIFT_SAMPLE = int(SAMPLE_RATE * FRAME_SHIFT_MS / 1000)
+# Frames per second (1000 / 10 = 100)
 FRAME_PER_SECONDS = int(1000 / FRAME_SHIFT_MS)
 
-# Dynamically compute HOP_SIZE (in samples)
+# Frame length in samples (16000 * 25 / 1000.0 = 400)
 FRAME_LENGTH_SAMPLES = int(SAMPLE_RATE * FRAME_LENGTH_MS / 1000.0)
+# Hop size in samples (16000 * 10 / 1000.0 = 160)
 HOP_SIZE = int(SAMPLE_RATE * FRAME_SHIFT_MS / 1000.0)
 
 # Example values:
