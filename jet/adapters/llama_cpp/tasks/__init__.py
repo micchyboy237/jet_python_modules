1afe6ebdb152c25277baa9fcafef5337b134410b
Jethro Estrada <jethroestrada237@gmail.com>
2026-09-09 07:17:38 +0800

    Updates

---
+++ jet/adapters/llama_cpp/tasks/__init__.py
@@ -0,0 +1,16 @@
+from .basic.answer_multiple_choice import answer_multiple_choice
+from .basic.answer_multiple_choice_with_context import (
+    answer_multiple_choice_with_context,
+)
+from .basic.answer_multiple_choice_with_key import answer_multiple_choice_with_key
+from .basic.answer_multiple_labels_with_context import (
+    answer_multiple_labels_with_context,
+)
+from .basic.classify_yes_no import classify_yes_no
+from .basic.evaluate_context_relevance import evaluate_context_relevance
+from .basic.evaluate_multiple_contexts_relevance import (
+    evaluate_multiple_contexts_relevance,
+)
+from .basic.evaluate_rag_relevance import evaluate_rag_relevance
+from .basic.evaluate_relevance import evaluate_relevance
+from .basic.evaluate_response_relevance import evaluate_response_relevance
